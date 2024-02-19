409ea6156c798359649c23557d75343923175a3c
Ravil <asimovravil06@gmail.com>
2024-02-19 12:37:34 +0600

    [PL] PLTabController

---
+++ Pilot logbook/PLTabController.swift
@@ -0,0 +1,71 @@
+//
+//  PLTabController.swift
+//  Pilot logbook
+//
+//  Created by Ravil on 19.02.2024.
+//
+
+import UIKit
+
+final class PLTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        addedTabBarSetup()
+        delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+        
+    private func addedTabBarSetup() {
+        let ta1VC = UINavigationController(rootViewController: PLFlightsController())
+        let ta2VC = UINavigationController(rootViewController: PL2Controller())
+        let ta3VC = UINavigationController(rootViewController: PL3Controller())
+        
+        tabBar.backgroundColor = UIColor(named: "background")
+        
+        UITabBar.appearance().shadowImage = UIImage()
+        UITabBar.appearance().backgroundImage = UIImage()
+        UITabBar.appearance().backgroundColor = UIColor(named: "background")
+        
+        ta1VC.tabBarItem = UITabBarItem(
+            title: "", image: UIImage(named: "pltab1")?.withRenderingMode(.alwaysOriginal),
+            selectedImage: UIImage(named: "pltab1Ac")?.withRenderingMode(.alwaysOriginal)
+        )
+
+        ta2VC.tabBarItem = UITabBarItem(
+            title: "", image: UIImage(named: "pltab2")?.withRenderingMode(.alwaysOriginal),
+            selectedImage: UIImage(named: "pltab2")?.withRenderingMode(.alwaysOriginal)
+        )
+        
+        ta3VC.tabBarItem = UITabBarItem(
+            title: "", image: UIImage(named: "pltab3")?.withRenderingMode(.alwaysOriginal),
+            selectedImage: UIImage(named: "pltab3Ac")?.withRenderingMode(.alwaysOriginal)
+        )
+        
+        viewControllers = [ta1VC, ta2VC, ta3VC]
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+    }
+}
+
+extension PLTabController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        if let viewControllerIndex = tabBarController.viewControllers?.firstIndex(of: viewController), viewControllerIndex == 1 {
+            let expenseVC = PL2Controller()
+
+            let ta3VC = UINavigationController(rootViewController: expenseVC)
+            present(ta3VC, animated: true, completion: nil)
+            return false
+        }
+        return true
+    }
+}
